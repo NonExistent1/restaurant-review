86a3e1089c49f8e89d59904fc532b589777a9844
Jordyn Kuhn <jkuhn2472@mail.kvcc.edu>
2024-02-09 19:33:24 -0500

    Add Restaurant and Review Model, Add Restaurant List View.

--- review/models.py
+++ review/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Review(models.Model):
+    """Review model class"""
+
+    restaurant = models.ForeignKey(
+        "Restaurant",
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+
+    author = models.ForeignKey(
+        "auth.User",
+        on_delete=models.CASCADE,
+        related_name="posts"
+    )
+
+    body = models.TextField()
+    rating = models.PositiveSmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String Method"""
+        return self.body
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    updated= models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """String Method"""
+        return self.name
+    
+    def get_absolute_url(self):
+        """Get the absolute URL for a single Post"""
+        return reverse("post_detail", kwargs={"pk": self.pk})
--- review/views.py
+++ review/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import Restaurant, Review
+
+class RestaurantListView(ListView):
+    """Restaurant List View"""
+    model = Restaurant
+    template_name = "home.html"
+
+class RestaurantReviewListView(DetailView):
+    """Review List View"""
+    model = Review
+    template_name = ""
